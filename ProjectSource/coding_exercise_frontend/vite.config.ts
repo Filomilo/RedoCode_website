780105489588b073936d0385881752864a80d0e6
Filomilo <60512955+Filomilo@users.noreply.github.com>
2024-01-03 23:50:30 +0100

    added docker for vue app

--- ProjectSource/coding_exercise_frontend/vite.config.ts
+++ ProjectSource/coding_exercise_frontend/vite.config.ts
@@ -12,7 +12,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
+  },
+  server: {
+      hmr: {host: '0.0.0.0'}
   }
 })
