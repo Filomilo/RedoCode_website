655f89517948e0c34d584c9f06103c42b1c35505
Filomilo <s092686@student.tu.kielce.pl>
2024-01-17 18:57:48 +0100

    created basic excesice tav\ble view in vue

---
+++ ProjectSource/RedoCode_frontend/src/components/ExcersiceTable.vue
@@ -0,0 +1,46 @@
+<template>
+    <b-table 
+    striped hover 
+    :items="data"
+    :fields="fields"
+    
+    >
+
+<template #cell(actions)="{item}">
+    <b-button 
+    pill
+    @click="onRowClick(item.id)"
+    variant="primary"
+    >
+        try
+    </b-button>
+</template>
+</b-table>
+
+</template>
+
+
+
+<script setup lang="ts">
+import type ExerciseType from "@/types/ExerciseType";
+import type OnTableClickType from "@/types/OnTableClickType"
+
+const fields: any[]= [
+    {key: 'name'}, 
+{
+    key: 'language',
+    label: 'language'
+},
+ {key: 'difficulty'},
+  {key: 'actions',
+    label: ''
+}
+];
+defineProps<{
+  data: ExerciseType[],
+  onRowClick: OnTableClickType
+}>()
+
+
+
+</script>
