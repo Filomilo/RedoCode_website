745a57d7e42a762517de209992611ef3bea455e8
Filomilo <s092686@student.tu.kielce.pl>
2024-01-18 22:21:32 +0100

    added basic axios

--- ProjectSource/RedoCode_frontend/src/views/ExcersiceView.vue
+++ ProjectSource/RedoCode_frontend/src/views/ExcersiceView.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>User {{ $route.params.id }}</div>
+   <div>
+    {{ text}}
+   </div>
 </template>
+
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+const text=ref('');
+
+
+
+
+onMounted(()=>{
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (text.value = response.data));
+})
+
+
+</script>
