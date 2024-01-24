3a320707e4c2ef8f557526f797a8019bbb1d8d3d
Filomilo <s092686@student.tu.kielce.pl>
2024-01-24 17:31:46 +0100

    added basic users from data base retrieval trough http request

--- ProjectSource/RedoCode_backend/src/main/java/com/redocode/backend/RedoCodeBackendApplication.java
+++ ProjectSource/RedoCode_backend/src/main/java/com/redocode/backend/RedoCodeBackendApplication.java
@@ -2,8 +2,10 @@ package com.redocode.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.redocode.backend.database")
 public class RedoCodeBackendApplication {
 
     public static void main(String[] args) {
---
+++ ProjectSource/RedoCode_backend/src/test/java/com/redocode/backend/databse/UserRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.redocode.backend.databse;
+
+import com.redocode.backend.database.UsersRepository;
+import com.redocode.backend.database.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    public void testFindByUserName() {
+        String userNameTest="test";
+        User user = new User(userNameTest);
+        usersRepository.save(user);
+        User foundUser = usersRepository.findByUserName(userNameTest);
+        assertNotNull(foundUser);
+        assertEquals(userNameTest, foundUser.getUserName());
+    }
+}
