aea4f0231fdd598bdcaef624719b7bab472236cb
Filomilo <fborowiec@wp.pl>
2024-02-15 18:23:18 +0100

    created base for establishing connection with with code runners in forntEnd

--- ProjectSource/RedoCode_backend/src/main/java/com/redocode/backend/StompConnectionControl/DisconnectEventListener.java
+++ ProjectSource/RedoCode_backend/src/main/java/com/redocode/backend/StompConnectionControl/DisconnectEventListener.java
@@ -1,21 +1,21 @@
-package com.redocode.backend;
+package com.redocode.backend.EventListiners;
 
+import com.redocode.backend.RedoCodeController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.stereotype.Component;
 
-
+@Slf4j
 @Component
 public class DisconnectEventListener implements ApplicationListener<SessionDisconnectEvent> {
 
     @Override
     public void onApplicationEvent(SessionDisconnectEvent event) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
-
-        String sessionId = accessor.getSessionId();
-        String username = accessor.getUser().getName(); // You can retrieve the username if you have set it during the connection
-
-        System.out.println("User disconnected: " + username + ", SessionId: " + sessionId);
+        String userName=accessor.getUser().getName();
+        log.info("User "+ userName+ " diconnected");
+        RedoCodeController.getInstance().removeConnectedUser(userName);
     }
 }
