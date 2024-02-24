f5c1913d275283d96d86fd71dfbd7e69494fec39
Filomilo <fborowiec@wp.pl>
2024-02-24 22:02:32 +0100

    fix vue deveolpemnt test

--- ProjectSource/RedoCode_frontend/cypress/e2e/dev/open_excersice.cy.ts
+++ ProjectSource/RedoCode_frontend/cypress/e2e/dev/open_excersice.cy.ts
@@ -5,7 +5,7 @@ describe('open first exercise', () => {
     cy.visit('http://localhost:4173/')
     cy.get('#Excersices_button > b-nav-text:nth-child(1)').click()
     cy.url().should('eq', 'http://localhost:4173/Exercises');
-    cy.get('tr.excercise-table-row:nth-child(1) > th:nth-child(1)').click()
+    cy.get('html > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(5) > button:nth-child(1) > svg:nth-child(1)').click()
     cy.url().should('eq', 'http://localhost:4173/Exercises/1');
     })
 })
