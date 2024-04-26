54bcff16e9fcbf40d4e11122910cecc31988d171
Filomilo <fborowiec@wp.pl>
2024-04-26 08:15:17 +0200

    fix js hell world test

--- ProjectSource/RedoCode_frontend/cypress/e2e/prod/HelloWorldJsRun.cy.ts
+++ ProjectSource/RedoCode_frontend/cypress/e2e/prod/HelloWorldJsRun.cy.ts
@@ -1,23 +1,19 @@
 describe('template spec', () => {
   it('passes', () => {
-    describe('template spec', () => {
-      it('passes', () => {
-          Cypress.on('uncaught:exception', (err, runnable) => {
-            return false
-        })
-        cy.visit('/')
-        cy.get("a.TopBarItemContainer:nth-child(2)").click()
-        cy.url().should('eq', Cypress.config().baseUrl+'/PlayGround');
-        cy.get('#pv_id_5').click()   
-        cy.get('#pv_id_5_1').click()
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(1000);
-        cy.get('.p-button-label').click()
-        cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeResultContainer div.EngineStatusContianer div.EngineStatusPanel div.EngineStatusStatus').contains('ACTIVE')
-        cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeEditorContainer div div div.monaco-editor.no-user-select.showUnused.showDeprecated.vs-dark div.overflow-guard div.monaco-scrollable-element.editor-scrollable.vs-dark div.lines-content.monaco-editor-background div.view-lines.monaco-mouse-cursor-text div.view-line').type("console.log('Hello World!')")
-        cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeEditorPanelSetting div.CodeEditorPlayButton button.p-button.p-component svg').click()
-        cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeResultContainer div.ConsoleResultConsoleCOntainer div.ConsoleResultConsoleCOntainerText').contains(">> Hello World!")
-        })
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        return false
     })
-  })
+    cy.visit('/')
+    cy.get("a.TopBarItemContainer:nth-child(2)").click()
+    cy.url().should('eq', Cypress.config().baseUrl+'/PlayGround');
+    cy.get('#pv_id_5').click()   
+    cy.get('#pv_id_5_1').click()
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
+    cy.get('.p-button-label').click()
+    cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeResultContainer div.EngineStatusContianer div.EngineStatusPanel div.EngineStatusStatus').contains('ACTIVE')
+    cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeEditorContainer div div div.monaco-editor.no-user-select.showUnused.showDeprecated.vs-dark div.overflow-guard div.monaco-scrollable-element.editor-scrollable.vs-dark div.lines-content.monaco-editor-background div.view-lines.monaco-mouse-cursor-text div.view-line').type("console.log('Hello World!')\n")
+    cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeEditorPanelSetting div.CodeEditorPlayButton button.p-button.p-component svg').click()
+    cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main#ContentConatiner.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeResultContainer div.ConsoleResultConsoleCOntainer div.ConsoleResultConsoleCOntainerText').contains(">> Hello World!")
+    })
 })
