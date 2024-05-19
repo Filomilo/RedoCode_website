a4a586453b024bb6407af044df475985c47a3b0a
Filomilo <fborowiec@wp.pl>
2024-05-19 12:47:34 +0200

    removed duplicate import

--- ProjectSource/RedoCode_frontend/src/main.ts
+++ ProjectSource/RedoCode_frontend/src/main.ts
@@ -47,8 +47,6 @@ import ConfirmDialog from 'primevue/confirmdialog'
 import ConfirmationService from 'primevue/confirmationservice'
 import './interceptors/axios'
 import ToastService from 'primevue/toastservice'
-import TabView from 'primevue/tabview';
-import TabPanel from 'primevue/tabpanel';
 const app = createApp(App)
 if (import.meta.env.MODE === 'development') {
   const { makeServer } = await import('./server')
@@ -82,8 +80,6 @@ app.component('Dialog', Dialog)
 app.component('Dialog', Dialog)
 app.component('ConfirmDialog', ConfirmDialog)
 app.component('Toast', Toast)
-app.component('TabView', TabView)
-app.component('TabPanel', TabPanel)
 app.use(ConfirmationService)
 app.use(ToastService)
 const pinia = createPinia()
