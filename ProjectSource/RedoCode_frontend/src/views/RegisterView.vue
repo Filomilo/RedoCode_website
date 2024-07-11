38fc8e326ba805323bdee5066928e951ff55a72d
Filomilo <fborowiec@wp.pl>
2024-07-11 22:49:26 +0200

    added vualidate  packageto front end

--- ProjectSource/RedoCode_frontend/src/views/RegisterView.vue
+++ ProjectSource/RedoCode_frontend/src/views/RegisterView.vue
@@ -7,29 +7,38 @@
       <div class="LoginPanelConatiner">
         <InputText
           type="text"
-          v-model="inputMail"
+
+          v-model="v$.email.$model"
           class="TextInputConainer AuthPanelElement"
           ca
           placeholder="e-mail"
         />
+        <div v-if="v$.email.$error">
+  {{ v$.email.$errors[0].$message }}
+</div>
+        <!-- <div :v-if="v$.email.$silentErrors.length>0"> -->
+      
+            <!-- {{ validation.email.$silentErrors[0].$message }} -->
+        <!-- </div> -->
         <InputText
           type="text"
-          v-model="inputLogin"
+          v-model="dataState.nickname"
           class="TextInputConainer AuthPanelElement"
           ca
           placeholder="nick"
         />
         <Password
-          v-model="inputPass"
+          v-model="dataState.password"
           class="TextInputConainer AuthPanelElement passwordCont"
           placeholder="password"
+          toggleMask 
         />
         <Password
-          v-model="inputPassRepeat"
+          v-model="dataState.repeatPassword"
           class="TextInputConainer AuthPanelElement passwordCont"
           placeholder="repeat password"
         />
-
+    
         <div class="AuthPanelElement boldText">Already have have an account?</div>
         <router-link to="/" class="AuthPanelElement linkText"> sign in </router-link>
         <Button class="BasicButton" label="Sign up" />
@@ -41,11 +50,30 @@
 
 <script setup lang="ts">
 import InputText from 'primevue/inputtext'
-import { ref } from 'vue'
+import { ref,computed,reactive,watch } from 'vue'
 import BasicButton from '@/components/BasicButton.vue'
 import ExcerciseListPromotion from '../components/ExcerciseListPromotion.vue'
-const inputMail = ref('')
-const inputLogin = ref('')
-const inputPass = ref('')
-const inputPassRepeat = ref('')
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+
+const dataState=reactive({
+  email: "",
+  nickname: "",
+  password: "",
+  repeatPassword: ""
+});
+
+const rules=()=>{return{
+  email:{ required,email },
+  nickname: "",
+  password: "",
+  repeatPassword: ""
+}};
+const v$ = useVuelidate(rules, dataState)
+
+// watch(dataState,()=>{
+//   console.log("chnage")
+//   v$.value.$validate;
+// });
+
 </script>
