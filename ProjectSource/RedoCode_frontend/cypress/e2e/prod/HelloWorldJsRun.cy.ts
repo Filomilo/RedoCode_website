2921e6aa11a7f0d52cacc06b1cc28f63b4431972
Filomilo <fborowiec@wp.pl>
2024-08-03 23:27:34 +0200

    foxed js hello test

--- ProjectSource/RedoCode_frontend/cypress/e2e/prod/HelloWorldJsRun.cy.ts
+++ ProjectSource/RedoCode_frontend/cypress/e2e/prod/HelloWorldJsRun.cy.ts
@@ -3,18 +3,23 @@ describe('template spec', () => {
       Cypress.on('uncaught:exception', (err, runnable) => {
         return false
     })
+    const codeEditorSequance='#coderunner-editor-panel textarea';
+
     cy.visit('/')
     cy.get("#switch-playground").click()
     cy.url().should('eq', Cypress.config().baseUrl+'/PlayGround');
     cy.get('#coderunner-dropdown').click()   
     cy.get('#coderunner-dropdown_1').click()
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1000);
+
     cy.get('.p-button-label').click()
-    cy.get("html body div#app html div#MainPageContainer div.BackGroundContainer main.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeResultContainer div.EngineStatusContianer div.EngineStatusPanel div.EngineStatusStatus").contains('ACTIVE')
-    cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeEditorContainer div div div.monaco-editor.no-user-select.showUnused.showDeprecated.vs-dark div.overflow-guard div.monaco-scrollable-element.editor-scrollable.vs-dark').type("console.log(\"Hello World!\")", { force: true })
+    // cy.get("PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeResultContainer div.EngineStatusContianer div.EngineStatusPanel div.EngineStatusStatus").contains('ACTIVE')
+    cy.get("#coderunner-loading-dialog").should('not.exist');
+    cy.get(codeEditorSequance).focus();
+    cy.get(codeEditorSequance).type("{selectAll}", { force: true });
+    cy.get(codeEditorSequance).type("console.log(\"Hello World!\")", { force: true })
     cy.get('#coderunner-run-button').click()
-    cy.wait(10000);
+
     cy.get('html body div#app html div#MainPageContainer div.BackGroundContainer main.PlayGroundBase div.heightLimit div.p-splitter.p-component.p-splitter-horizontal div.p-splitter-panel div.CodeResultContainer div.ConsoleResultConsoleCOntainer div.ConsoleResultConsoleCOntainerText').contains("Hello World!")
     })
 })
