920ef4a1d1d488077267c547fa0a778a5762de74
Filomilo <fborowiec@wp.pl>
2024-08-09 20:39:13 +0200

    tests cepc pattern

--- ProjectSource/RedoCode_frontend/cypress.config.ts
+++ ProjectSource/RedoCode_frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/prod/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    specPattern: 'src/__tests__/e2e/prod/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost',
     defaultCommandTimeout: 100000,
     supportFile: false,
