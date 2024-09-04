6f847b59291ebe4ba26956ddb8da287462bfebe9
Filomilo <fborowiec@wp.pl>
2024-09-04 22:18:50 +0200

    fix build

--- ProjectSource/RedoCode_frontend/vite.config.ts
+++ ProjectSource/RedoCode_frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,8 +23,13 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
+         
           if (id.includes('src/components')) {
-            return 'components'
+          //   const splits=id.split("/")
+
+          
+          // return `components/${splits[splits.length-1]}`;
+          return 'components'
           }
           if (id.includes('src/controllers')) {
             return 'controllers'
