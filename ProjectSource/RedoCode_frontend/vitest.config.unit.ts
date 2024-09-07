363b17382f101fc0fb59aa7660245ebf5b4436ed
Filomilo <fborowiec@wp.pl>
2024-09-07 08:14:40 +0200

    fix vitest conifg

--- ProjectSource/RedoCode_frontend/vitest.config.unit.ts
+++ ProjectSource/RedoCode_frontend/vitest.config.unit.ts
@@ -1,20 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
 
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import { resolve } from 'path'
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  },
-  test: {
-    environment: 'jsdom',
-    include: ['src/__tests__/unit/**/*.spec.ts'],
-    exclude: ['e2e/*'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
-  },
-})
+  })
+)
