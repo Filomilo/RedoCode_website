f7a4afae94eff8539c15ea6a6c919f0a047ac61c
Filomilo <fborowiec@wp.pl>
2024-09-14 07:22:08 +0200

    debug

--- ProjectSource/RedoCode_frontend/src/__tests__/e2e/prod/AuthenticatedFibonachi.cy.ts
+++ ProjectSource/RedoCode_frontend/src/__tests__/e2e/prod/AuthenticatedFibonachi.cy.ts
@@ -174,18 +174,6 @@ describe.skip('Phibonachi Auhtenticated new exercise', () => {
     ExercisesPage.shouldBeOnUrlOfExerciseId(1)
     CodeRunnerPanel.selectInitialLanguage('cpp')
     CodeRunnerPanel.stateShouldBe('ACTIVE')
-    CodeRunnerPanel.CodeRunnerInput.codeRunnerShouldContain(
-      'int solution(int x){'
-    )
-    CodeRunnerPanel.CodeRunnerInput.clearCodeRunner()
-    CodeRunnerPanel.information.nameShould('fibonachi sequance')
-    CodeRunnerPanel.information.descriptionShouldBe(
-      'Create funciton that returns number at point of fibocnahi squnace so 1->0 2->1 3->1 4->2 5->3 and do on'
-    )
-    CodeRunnerPanel.CodeRunnerInput.inputToCodeRunner('test')
-    CodeRunnerPanel.shouldSubmitAccess(false)
-    CodeRunnerPanel.run()
-    CodeRunnerPanel.Tests.shouldAllTestFail(1)
     CodeRunnerPanel.CodeRunnerInput.clearCodeRunner()
     CodeRunnerPanel.CodeRunnerInput.inputToCodeRunner(cppSolution)
     CodeRunnerPanel.run()
