2ab2af8f050d7488cdb6fa9dd2806f3f7e212854
Filomilo <fborowiec@wp.pl>
2024-09-18 22:47:08 +0200

    chekicng vresion iwth js

--- ProjectSource/RedoCode_frontend/src/__tests__/e2e/prod/HelloWorldCppRun.cy.ts
+++ ProjectSource/RedoCode_frontend/src/__tests__/e2e/prod/HelloWorldCppRun.cy.ts
@@ -2,18 +2,36 @@ import CodeRunnerPanel from '../helpers/CodeRunnerPanel'
 import SwitcherControls from '../helpers/SwitcherControls'
 import UrlControls from '../helpers/UrlControls'
 
-describe('hello world cpp run ', () => {
+// describe('hello world cpp run ', () => {
+//   it('passes', () => {
+//     Cypress.on('uncaught:exception', (err, runnable) => {
+//       return false
+//     })
+//     UrlControls.startPage()
+//     SwitcherControls.switchPlayground()
+//     CodeRunnerPanel.selectInitialLanguage('cpp')
+//     CodeRunnerPanel.CodeRunnerInput.clearCodeRunner()
+//     CodeRunnerPanel.CodeRunnerInput.inputToCodeRunner(
+//       '#include <iostream>\n\n int main(){\n std::cout<<"Hello World!";\n return 0;\n}'
+//     )
+//     CodeRunnerPanel.run()
+//     CodeRunnerPanel.RawCodeResults.shouldResultBe('Hello World!')
+//   })
+// })
+describe('hello world js run ', () => {
   it('passes', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
       return false
     })
+
+    const helloWorldProgram = 'console.log("Hello World!")'
+
     UrlControls.startPage()
+
     SwitcherControls.switchPlayground()
-    CodeRunnerPanel.selectInitialLanguage('cpp')
+    CodeRunnerPanel.selectInitialLanguage('js')
     CodeRunnerPanel.CodeRunnerInput.clearCodeRunner()
-    CodeRunnerPanel.CodeRunnerInput.inputToCodeRunner(
-      '#include <iostream>\n\n int main(){\n std::cout<<"Hello World!";\n return 0;\n}'
-    )
+    CodeRunnerPanel.CodeRunnerInput.inputToCodeRunner(helloWorldProgram)
     CodeRunnerPanel.run()
     CodeRunnerPanel.RawCodeResults.shouldResultBe('Hello World!')
   })
