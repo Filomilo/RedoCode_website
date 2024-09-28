e2e2b715786b5ade69cd0b4262f1dd349e156291
Filomilo <fborowiec@wp.pl>
2024-09-28 21:19:01 +0200

    optimized buiold options

--- ProjectSource/RedoCode_frontend/vite.config.ts
+++ ProjectSource/RedoCode_frontend/vite.config.ts
@@ -3,7 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+function getFolder(id:string){
+    if(id===undefined)
+    return ''
+  const splits=id.split("/");
+  return splits[splits.length-2];
+}
 
+function getFileName(id:string){
+    if(id===undefined)
+    return ''
+  const splits=id.split("/");
+  return splits[splits.length-1].split(".")[0];
+}
+function getExtension(id:string){
+  if(id===undefined)
+    return ''
+  const splits=id.split("/");
+  return splits[splits.length-1].split(".")[1];
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
@@ -17,11 +35,45 @@ export default defineConfig({
   //   drop: ['console', 'debugger'],
   // },
 
+
+
   build: {
+    modulePreload: {
+      resolveDependencies: () => [],
+    },
     // minify: false,
     rollupOptions: {
       output: {
         manualChunks(id) {
+          console.log("getFileName: "+id)
+
+          const folder=getFolder(id);
+          const name=getFileName(id);
+          const extension=getExtension(id);
+
+          // console.log("folder: "+folder);
+          // console.log("name: "+name);
+          // console.log("extension: "+extension);
+          if (id.includes('node_modules')) {
+            if (id.includes('@vue')) return 'vendor/vendor-vue';
+            if (id.includes('primevue')) {
+              return   'vendor/primevue/vendor-primevue-'+folder
+            }
+            if (id.includes('chart')) return 'vendor/vendor-chart';
+
+            return 'vendor/vendor';
+        }
+
+          if(folder==='components')
+          {
+            return 'components';
+          }
+            
+          if(folder==='views')
+            {
+              return 'views/'+name;
+            }
+
           if (id.includes('src/components')) {
             return 'components'
           }
@@ -31,6 +83,8 @@ export default defineConfig({
           if (id.includes('src/types')) {
             return 'types'
           }
+          
+          
         },
       },
     },
