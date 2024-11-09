412adec0be97eba5aa896397ee9fcf022ff59919
Filomilo <60512955+Filomilo@users.noreply.github.com>
2024-11-09 18:08:20 +0000

    added coloe changes (#129)

--- ProjectSource/RedoCode_frontend/src/components/TopBar.vue
+++ ProjectSource/RedoCode_frontend/src/components/TopBar.vue
@@ -1,15 +1,17 @@
 <template>
   <nav class="">
     <div class="TopBarContainer">
+      <!-- {{ route }} -->
       <div class="TopBarList">
-        <router-link to="/Home" class="TopBarItemContainer" id="switch-home">
+        <router-link to="/Home" :class="`TopBarItemContainer ${isHomeSelected ? 'selected' : ''}`" id="switch-home"
+        >
           <IconHome class="TopBarItemIcon" />
           <label class="TopBarItemTitle">Home</label>
         </router-link>
 
         <router-link
           to="/PlayGround"
-          class="TopBarItemContainer"
+         :class="`TopBarItemContainer ${isPlaygroundSelected ? 'selected' : ''}`"
           id="switch-playground"
         >
           <IconPlayground class="TopBarItemIcon" />
@@ -18,7 +20,7 @@
 
         <router-link
           to="/Exercises"
-          class="TopBarItemContainer"
+         :class="`TopBarItemContainer ${isExercisesSelected ? 'selected' : ''}`"
           id="switch-exercises"
         >
           <IconExcerisce class="TopBarItemIcon" />
@@ -27,7 +29,7 @@
 
         <router-link
           to="/Account"
-          class="TopBarItemContainer"
+         :class="`TopBarItemContainer ${isAccountSelected ? 'selected' : ''}`"
           id="switch-account"
           v-if="activeUserStore.isLogged"
         >
@@ -46,6 +48,28 @@
   import IconExcerisce from '@/assets/icons/IconExcerisce.vue'
   import IconAccount from '@/assets/icons/IconAccount.vue'
   import { useActiveUserStore } from '../stores/ActiveUserStore'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
   const activeUserStore = useActiveUserStore()
+  const route=useRoute();
+  const isHomeSelected=computed(()=>{
+    return route.fullPath.includes("/Home");
+  })
+
+  const isPlaygroundSelected=computed(()=>{
+    return route.fullPath.includes("/PlayGround");
+  })
+
+  const isExercisesSelected=computed(()=>{
+    return route.fullPath.includes("/Exercises");
+  })
+  
+  const isAccountSelected=computed(()=>{
+    return route.fullPath.includes("/Account");
+  })
 </script>
+
+<style lang="css">
+
+</style>
