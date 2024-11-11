ee3565e58b9e4831f95675481b7cc3b106dc56f5
Filomilo <fborowiec@wp.pl>
2024-11-11 19:37:12 +0100

    fix type casting

--- ProjectSource/RedoCode_frontend/src/__tests__/e2e/prod/SwitchingBetwenCoderunners.cy.ts
+++ ProjectSource/RedoCode_frontend/src/__tests__/e2e/prod/SwitchingBetwenCoderunners.cy.ts
@@ -1,32 +1,47 @@
 import CodeRunnerPanel from '../helpers/CodeRunnerPanel'
 import SwitcherControls from '../helpers/SwitcherControls'
 import UrlControls from '../helpers/UrlControls'
-// test doenst make ene needs fixing later
-describe('hello world cpp run ', () => {
-  it('passes', () => {
+
+
+const helloWorldProgramCPP = `
+#include<iostream>
+int main()
+{
+std::cout<<"Hello World!";
+return 0;
+}
+`;
+
+const helloWorldProgramJS = 'console.log("Hello World!")'
+
+describe('Switching between code runners ', () => {
+  it('swtiching between pages', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
       if (err.message.includes('ResizeObserver')) {
         return false
       }
     })
     cy.reload()
-    const helloWorldProgram = `
-    #include<iostream>
-    int main()
-    {
-    std::cout<<"Hello World!";
-    return 0;
-    }
-    `;
 
     UrlControls.startPage()
 
+    //JS HELLO WORLD
     SwitcherControls.switchPlayground()
-    CodeRunnerPanel.selectInitialLanguage('cpp')
+    CodeRunnerPanel.selectInitialLanguage('js')
     CodeRunnerPanel.CodeRunnerInput.clearCodeRunner()
-    CodeRunnerPanel.CodeRunnerInput.inputToCodeRunner(helloWorldProgram)
+    CodeRunnerPanel.CodeRunnerInput.inputToCodeRunner(helloWorldProgramJS)
     CodeRunnerPanel.RawCodeResults.shouldResultBe('')
     CodeRunnerPanel.run()
     CodeRunnerPanel.RawCodeResults.shouldResultBe('Hello World!')
+  
+  
+  //Swtich Around
+  SwitcherControls.switchExercises();
+  SwitcherControls.switchHome();
+  SwitcherControls.switchExercises();
+  SwitcherControls.switchPlayground();
+
+  
+  
   })
 })
